e6b7ca1c64976d3008b8decc5661be97c2266cd1
Nathan Voxland <nathan@voxland.net>
2013-11-19 13:48:40 -0600

    Missed priority and supports

--- src/main/java/liquibase/ext/redshift/datatype/DateTimeTypeRedshift.java
+++ src/main/java/liquibase/ext/redshift/datatype/DateTimeTypeRedshift.java
@@ -2,8 +2,20 @@ package liquibase.ext.redshift.datatype;
 
 import liquibase.database.Database;
 import liquibase.datatype.DatabaseDataType;
+import liquibase.ext.redshift.database.RedshiftDatabase;
 
 public class DateTimeTypeRedshift extends liquibase.datatype.core.DateTimeType {
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
+    }
+
+    @Override
+    public boolean supports(Database database) {
+        return database instanceof RedshiftDatabase;
+    }
+
     @Override
     public DatabaseDataType toDatabaseDataType(Database database) {
         return new DatabaseDataType("TIMESTAMP");
