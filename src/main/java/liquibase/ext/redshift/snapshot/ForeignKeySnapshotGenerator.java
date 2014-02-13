ff26cbdafcdedcbfdbb13e8e09715622a6f0040a
Nathan Voxland <nathan@voxland.net>
2014-02-13 16:47:18 -0600

    Redshift driver does not support getting incoming keys. Does not really support FKs anyway so making FK snapshot a no-op

---
+++ src/main/java/liquibase/ext/redshift/snapshot/ForeignKeySnapshotGenerator.java
@@ -0,0 +1,24 @@
+package liquibase.ext.redshift.snapshot;
+
+import liquibase.database.Database;
+import liquibase.exception.DatabaseException;
+import liquibase.snapshot.DatabaseSnapshot;
+import liquibase.snapshot.InvalidExampleException;
+import liquibase.structure.DatabaseObject;
+
+public class ForeignKeySnapshotGenerator extends liquibase.snapshot.jvm.ForeignKeySnapshotGenerator {
+    @Override
+    public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
+        return PRIORITY_DATABASE;
+    }
+
+    @Override
+    protected void addTo(DatabaseObject foundObject, DatabaseSnapshot snapshot) throws DatabaseException, InvalidExampleException {
+        //Redshift does not support calls to MetaData.getIncomingKeys() and doesn't really support FKs anyway
+    }
+
+    @Override
+    protected DatabaseObject snapshotObject(DatabaseObject example, DatabaseSnapshot snapshot) throws DatabaseException, InvalidExampleException {
+        return null; //Redshift does not support calls to MetaData.getIncomingKeys() and doesn't really support FKs anyway
+    }
+}
