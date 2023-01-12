e73177e48545d6c3bff7cecd3982f094f390be22
yodzhubeiskyi <oesevg@gmail.com>
2023-01-12 12:59:33 +0200

    Fixing test data

--- src/test/groovy/LiquibaseHarnessAdvancedSuiteTest.groovy
+++ src/test/groovy/LiquibaseHarnessAdvancedSuiteTest.groovy
@@ -1,4 +1,9 @@
 import liquibase.harness.AdvancedHarnessSuite
+import liquibase.harness.config.TestConfig
 
 class LiquibaseHarnessAdvancedSuiteTest extends AdvancedHarnessSuite {
+    static {
+        //extensions don't distribute their files. Only we store them in src/main/resources.
+        TestConfig.instance.outputResourcesBase = "src/main/resources"
+    }
 }
