574c0d134b8c063d38f55873cc71078409722dd8
yodzhubeiskyi <oesevg@gmail.com>
2023-01-17 17:21:33 +0100

    Fix

--- src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessFoundationalSuiteTest.groovy
+++ src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessFoundationalSuiteTest.groovy
@@ -3,7 +3,7 @@ package liquibase.ext.redshift
 import liquibase.harness.FoundationalHarnessSuite
 import liquibase.harness.config.TestConfig
 
-class LiquibaseHarnessFoundaionalSuiteTest extends FoundationalHarnessSuite {
+class LiquibaseHarnessFoundationalSuiteTest extends FoundationalHarnessSuite {
     static {
         //extensions don't distribute their files. Only we store them in src/main/resources.
         TestConfig.instance.outputResourcesBase = "src/main/resources"
