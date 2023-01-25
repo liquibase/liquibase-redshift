f768aa442add3a2f577e126fdc4765664fe6d61e
yodzhubeiskyi <oesevg@gmail.com>
2023-01-25 14:24:58 +0100

    Advanced test fix

--- src/test/resources/liquibase/harness/generateChangelog/expectedChangeLog/redshift/createTable.redshift.sql
+++
@@ -1,5 +0,0 @@
--- liquibase formatted sql
-
--- changeset yodzh:1674493560417-1
-CREATE TABLE "test_table_xml" ("test_column" INTEGER);
-
--- src/test/resources/liquibase/harness/generateChangelog/expectedChangeLog/redshift/createView.redshift.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
-
--- changeset yodzh:1674562081600-1
-CREATE TABLE "view_test_table" ("test_column" INTEGER);
-
--- changeset yodzh:1674562081600-2
-CREATE VIEW "test_view" AS SELECT view_test_table.test_column FROM view_test_table;;
-
