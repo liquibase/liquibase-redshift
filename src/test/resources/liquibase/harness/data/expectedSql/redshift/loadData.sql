70a789c67c292ec9a6849888458a9d6beda9e466
Vitalii Makarchenkov <mail@vitaliimak.com>
2023-04-19 18:32:55 +0300

    Fix ChangeDataTests

---
+++ src/test/resources/liquibase/harness/data/expectedSql/redshift/loadData.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.posts (id, author_id, title, description, content, inserted_date) VALUES (6, 1, 'Title1', 'test1@example.com', 'Content1', '2000-01-01'),(7, 2, 'Title2', 'test2@example.com', 'Content2', '2000-01-01'),(8, 3, 'Title3', 'test3@example.com', 'Content3', '2000-01-01')
