40dc2e3440fddf9044345be143563ad181822f06
Charles Queiroz <fcqueiroz@liquibase.com>
2025-04-02 15:21:05 +0100

    DAT-19655 - upgrade test-harness

--- src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessFoundationalSuiteTest.groovy
+++ src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessFoundationalSuiteTest.groovy
@@ -1,11 +1,7 @@
 package liquibase.ext.redshift
 
 import liquibase.harness.FoundationalHarnessSuite
-import liquibase.harness.config.TestConfig
 
-class LiquibaseHarnessFoundationalSuiteTest extends FoundationalHarnessSuite {
-    static {
-        //extensions don't distribute their files. Only we store them in src/main/resources.
-        TestConfig.instance.outputResourcesBase = "src/main/resources"
-    }
+class FoundationalExtensionHarnessSuite extends FoundationalHarnessSuite {
+
 }
