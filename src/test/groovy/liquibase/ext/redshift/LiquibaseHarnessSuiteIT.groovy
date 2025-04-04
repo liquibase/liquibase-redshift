a92c3a0c4c38c43a650dd1ebe6f2d6d93c1512f5
Charles Queiroz <fcqueiroz@liquibase.com>
2025-04-04 03:47:48 +0100

    DAT-19655 - upgrade test-harness

--- src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessSuiteIT.groovy
+++ src/test/groovy/liquibase/ext/redshift/LiquibaseHarnessSuiteIT.groovy
@@ -1,6 +1,14 @@
 package liquibase.ext.redshift
 
 import liquibase.harness.BaseHarnessSuite
+import liquibase.harness.config.TestConfig
+import liquibase.Scope
 
 class LiquibaseHarnessSuiteIT extends BaseHarnessSuite {
+    
+    def setupSpec() {
+        System.setProperty("inputFormat", "xml")
+        Scope.getCurrentScope().getUI().sendMessage("Running RedshiftHarnessSuite with input format: ${TestConfig.instance.inputFormat}")
+        Scope.getCurrentScope().getUI().sendMessage("Database configuration: ${TestConfig.instance.getFilteredDatabasesUnderTest()}")
+    }
 }
