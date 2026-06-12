cc51e7e65a048451a1206b4b65d4f26bb0c83797
HorbatenkoYehor <nelfan95@gmail.com>
2026-06-12 16:17:51 +0300

    INT-2049: Fix RedshiftTagDatabaseGenerator intercepting tag command for non-Redshift databases

--- src/main/java/liquibase/ext/redshift/sqlgenerator/core/RedshiftTagDatabaseGenerator.java
+++ src/main/java/liquibase/ext/redshift/sqlgenerator/core/RedshiftTagDatabaseGenerator.java
@@ -1,19 +1,15 @@
 package liquibase.ext.redshift.sqlgenerator.core;
 
 import liquibase.database.Database;
-import liquibase.database.core.*;
-import liquibase.sqlgenerator.core.TagDatabaseGenerator;
 import liquibase.datatype.DataTypeFactory;
 import liquibase.exception.ValidationErrors;
+import liquibase.ext.redshift.database.RedshiftDatabase;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
 import liquibase.sqlgenerator.SqlGeneratorChain;
-import liquibase.sqlgenerator.SqlGeneratorFactory;
+import liquibase.sqlgenerator.core.TagDatabaseGenerator;
 import liquibase.statement.core.TagDatabaseStatement;
-import liquibase.statement.core.UpdateStatement;
 import liquibase.structure.core.Column;
-import liquibase.structure.core.Table;
-import liquibase.ext.redshift.database.RedshiftDatabase;
 
 public class RedshiftTagDatabaseGenerator extends TagDatabaseGenerator {
 
@@ -25,31 +21,29 @@ public class RedshiftTagDatabaseGenerator extends TagDatabaseGenerator {
     }
 
     @Override
-    public int getPriority()
-    {
-        return 6;
+    public boolean supports(TagDatabaseStatement statement, Database database) {
+        return database instanceof RedshiftDatabase;
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
     }
 
     @Override
     public Sql[] generateSql(TagDatabaseStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        if (database instanceof RedshiftDatabase) {
-            String tableNameEscaped = database.escapeTableName(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName());
-            String orderColumnNameEscaped = database.escapeObjectName("ORDEREXECUTED", Column.class);
-            String dateColumnNameEscaped = database.escapeObjectName("DATEEXECUTED", Column.class);
-            String tagColumnNameEscaped = database.escapeObjectName("TAG", Column.class);
-            String tagEscaped = DataTypeFactory.getInstance().fromObject(statement.getTag(), database).objectToSql(statement.getTag(), database);
+        String tableNameEscaped = database.escapeTableName(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName());
+        String orderColumnNameEscaped = database.escapeObjectName("ORDEREXECUTED", Column.class);
+        String dateColumnNameEscaped = database.escapeObjectName("DATEEXECUTED", Column.class);
+        String tagColumnNameEscaped = database.escapeObjectName("TAG", Column.class);
+        String tagEscaped = DataTypeFactory.getInstance().fromObject(statement.getTag(), database).objectToSql(statement.getTag(), database);
 
-            return new Sql[]{
-                    new UnparsedSql(
-                            "UPDATE " + tableNameEscaped + " SET " + tagColumnNameEscaped + " = " + tagEscaped +
-                                    " FROM (SELECT " + dateColumnNameEscaped + ", " + orderColumnNameEscaped + " FROM " + tableNameEscaped + " ORDER BY " + dateColumnNameEscaped + " DESC, " + orderColumnNameEscaped + " DESC LIMIT 1) AS sub " +
-                                    "WHERE " + tableNameEscaped + "." + dateColumnNameEscaped + "=sub." + dateColumnNameEscaped + " AND " + tableNameEscaped + "." + orderColumnNameEscaped + "=sub." + orderColumnNameEscaped)
-            };
-        
-        }
-
-
-        return super.generateSql(statement, database, sqlGeneratorChain);
+        return new Sql[]{
+                new UnparsedSql(
+                        "UPDATE " + tableNameEscaped + " SET " + tagColumnNameEscaped + " = " + tagEscaped +
+                                " FROM (SELECT " + dateColumnNameEscaped + ", " + orderColumnNameEscaped + " FROM " + tableNameEscaped + " ORDER BY " + dateColumnNameEscaped + " DESC, " + orderColumnNameEscaped + " DESC LIMIT 1) AS sub " +
+                                "WHERE " + tableNameEscaped + "." + dateColumnNameEscaped + "=sub." + dateColumnNameEscaped + " AND " + tableNameEscaped + "." + orderColumnNameEscaped + "=sub." + orderColumnNameEscaped)
+        };
 
     }
 }
